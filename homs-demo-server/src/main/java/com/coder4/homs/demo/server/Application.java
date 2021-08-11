a0cdfd383bbf2eae1fe01d7275183be8f832cfd4
coder4 <liheyuan.bj@gmail.com>
2021-08-11 18:20:10 +0800

    MOD: Init commit

---
+++ homs-demo-server/src/main/java/com/coder4/homs/demo/server/Application.java
@@ -0,0 +1,22 @@
+/**
+ * @(#)Application.java, 8月 11, 2021.
+ * <p>
+ * Copyright 2021 coder4.com. All rights reserved.
+ * CODER4.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.coder4.homs.demo.server;
+
+/**
+ * @author coder4
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
